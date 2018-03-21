ad3a5afc0de3559bc3dc8ac457a239841dde22ea
Haoran Feng <harrifeng@gmail.com>
2018-03-21 17:41:48 +0800

    ok

---
+++ ch02/test_case_together.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Is it morning? Please answer yes or no"
+
+read timeofday
+
+case "$timeofday" in
+  yes | y | Yes | YES) echo "Good Morning";;
+  n* | N*            ) echo "Good Afternoon";;
+  *                  ) echo "Sorry, answer not recoginzed";;
+esac
